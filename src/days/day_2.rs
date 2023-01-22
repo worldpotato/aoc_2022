4dcc767c928bd543ba6dc852c17628a085c0ea42
worldpotato <mail@felixstrobel.de>
2023-01-22 20:46:19 +0100

    working on day2 part two

--- src/days/day_2.rs
+++ src/days/day_2.rs
@@ -2,6 +2,34 @@
 use crate::days::day_result::DayResult;
 use crate::input::*;
 
+use log::{debug, error, log_enabled, info, Level};
+
+/// Calculate the value from the given hand.
+///
+/// Expect either a-c or x - z
+fn calculate_value_from_input(input: u8) -> u8 {
+
+    
+    info!("input: {}", input);
+    // the offset for the chars a, b, c
+    let beginning_offset = 64;
+    // the offset for the chars x, y , z
+    let ending_offset = 87;
+
+    let mut hand_value = input - beginning_offset;
+
+
+    println!("hand: {}", hand_value);
+    // here we distingues between the begin and ending
+    if hand_value > 3 {
+        let diff = ending_offset - beginning_offset;
+        println!("{}", diff);
+        hand_value -= diff;
+    }
+
+    return hand_value;
+}
+
 /// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 ///
 /// In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
@@ -14,13 +42,13 @@ fn part_one(input: Option<String>) -> Result<String, String>{
     let scissor_win_loose = vec![results.loose + 3, results.win + 3, results.draw + 3];
     let lose_win_table = vec![rock_win_loose, paper_win_loose, scissor_win_loose];
 
-    // A = Rock = 1
-    // B = Paper = 2
-    // C = Scissors = 3
+    // a = rock = 1
+    // b = paper = 2
+    // c = scissors = 3
     //
-    // X = Rock
-    // Y = Paper
-    // Z = Scissors
+    // x = rock
+    // y = paper
+    // z = scissors
     //
     // lost = 0
     // draw = 3
@@ -69,19 +97,41 @@ fn part_one(input: Option<String>) -> Result<String, String>{
 /// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 ///
 fn part_two(input: Option<String>) -> Result<String, String>{
-    // let plain_input: String;
-    // let result: Result<String, String>;
+
+        
+    // let results = ResultPoints {win: 6, loose: 0, draw: 3};
     //
-    // match input {
-    //     Some(i) => plain_input = i,
-    //     None => {
-    //         // result = Err("No input given".to_string());
-    //         plain_input = "".to_string();
+    // let rock_win_loose = vec![results.draw + 1, results.loose + 1, results.win + 1];
+    // let paper_win_loose = vec![results.win + 2, results.draw + 2, results.loose + 2];
+    // let scissor_win_loose = vec![results.loose + 3, results.win + 3, results.draw + 3];
+    // let lose_win_table = vec![rock_win_loose, paper_win_loose, scissor_win_loose];
+    //
+    // // a = rock = 1
+    // // b = paper = 2
+    // // c = scissors = 3
+    // //
+    // // x = loose
+    // // y = draw
+    // // z = win
+    // //
+    // // lost = 0
+    // // draw = 3
+    // // won = 6
+    //
+    // let elf_offset = 64;
+    // let my_offset = 87;
+    //
+    // let plain_input = match input {
+    //     some(i) => i,
+    //     none => {
+    //         // result = err("no input given".to_string());
+    //         "no input".to_string()
     //     },
-    // }
+    // };
+    // let lines = plain_input.split('\n');
     //
-    let result = Ok("test".to_string());
-    return result;
+    let my_result: Result<String, String> = Ok(String::from("Test"));
+    return my_result;
 }
 
 pub fn day_two(input: Challenge) -> DayResult{
@@ -126,6 +176,30 @@ mod test_day_two {
         let input = complete_input.test;
         let day_one: Result<String, String> = super::part_two(input.one);
         assert_eq!(day_one.unwrap(), String::from("45000"));
+    }
+
+    #[test]
+    fn test_calculate_value_from_input(){
+        let input_a = "A";
+        let input_b = "B";
+        let input_c = "C";
+
+        let input_x = "X";
+        let input_y = "Y";
+        let input_z = "Z";
+        
+        let first_result = super::calculate_value_from_input(input_a.as_bytes()[0]);
+        assert_eq!(first_result, 1);
+        let second_result = super::calculate_value_from_input(input_b.as_bytes()[0]);
+        assert_eq!(second_result, 2);
+        let third_result = super::calculate_value_from_input(input_c.as_bytes()[0]);
+        assert_eq!(third_result, 3);
+        let fourth_result = super::calculate_value_from_input(input_x.as_bytes()[0]);
+        assert_eq!(fourth_result, 1);
+        let fifth_result = super::calculate_value_from_input(input_y.as_bytes()[0]);
+        assert_eq!(fifth_result, 2);
+        let sixed_result = super::calculate_value_from_input(input_z.as_bytes()[0]);
+        assert_eq!(sixed_result, 3);
     }
 }
 
