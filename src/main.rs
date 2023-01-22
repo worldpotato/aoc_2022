04ed78603773ebd66d4c2fdf610c1449140dbc7b
worldpotato <mail@felixstrobel.de>
2023-01-22 20:45:30 +0100

    add debug o macro

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
 pub mod days;
 pub mod parameters;
 pub mod input;
+pub mod modes;
+
+#[macro_use]
+extern crate log;
+
+use log::{debug, error, log_enabled, info, Level};
 
 use crate::days::*;
 use crate::input::*;
-
-pub enum Modes {
-    Challenge,
-    Test
-}
+use modes::Modes;
+// use parameters::parse_parameters;
+// use std::env;
 
 fn main() {
 
-    let day: u8 = 2;
+    env_logger::init();
+    let day: u8 = 1;
     let mode = Modes::Challenge;
     // let args: Vec<String> = env::args().collect();
     // let (day, mode) = parse_parameters(&args);
     let complete_input = parse_input(day);
     let input_value = complete_input.get_input(mode);
-    // mode.map(|n| n.
     let day_result = run_day(day, input_value);
 
     match day_result.part_one {
-        Ok(i) => println!("Result of part one: {}", i),
-        Err(i) => println!("{}", i),
+        Ok(i) => info!("Result of part one: {}", i),
+        Err(i) => error!("{}", i),
     }
 
     match day_result.part_two {
-        Ok(i) => println!("Result of part two: {}", i),
-        Err(i) => println!("{}", i),
+        Ok(i) => info!("Result of part two: {}", i),
+        Err(i) => error!("{}", i),
     }
 }
 
